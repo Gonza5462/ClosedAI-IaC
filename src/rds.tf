7644adee1115dceeb16aea30fb4f34ec4c13ab76
Gonzalo Da Costa <gdacosta@proveedor.teco.com.ar>
2024-05-07 09:29:02 -0300

    fix password

--- src/rds.tf
+++ src/rds.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "XCoin_DB" {
   engine_version       = "8.0.35"
   instance_class       = "db.t3.micro"
   username             = "xcoin"
-  password             = "xcoin"
+  password             = "xcoinclosedai"
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 }
