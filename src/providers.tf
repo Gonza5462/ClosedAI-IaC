d8f09dec0859da1a245851e3c2206842408dc20f
Gonzalo Da Costa <gdacosta@proveedor.teco.com.ar>
2024-05-07 01:25:49 -0300

    Fix de identacion

--- src/providers.tf
+++ src/providers.tf
@@ -1,16 +1,16 @@
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~>5.47.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.47.0"
     }
-    required_version = "~>1.8.0"
+  }
+  required_version = "~>1.8.0"
 }
 
 provider "aws" {
-    region = "us-east-1"
-    default_tags {
-      tags = var.tags
-    }
+  region = "us-east-1"
+  default_tags {
+    tags = var.tags
+  }
 }
--- src/s3.tf
+++ src/s3.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tfstates_bucket_e
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
   }
 }
