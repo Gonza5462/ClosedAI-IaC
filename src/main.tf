d80d7f845dcc6f05915aadaa368933569f4a13c3
Gonzalo Da Costa <gdacosta@proveedor.teco.com.ar>
2024-05-10 21:04:23 -0300

    fix dynamodb

--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "xcoin-closedai-tfstates-bucket"
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "terraform-state-locks"
     key            = "global/statefile/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
