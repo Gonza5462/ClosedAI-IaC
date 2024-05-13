7cfdbe92613b8ae1ad456451308c1a459682ed0f
Gonzalo Santiago Da Costa <gdacosta@uade.edu.ar>
2024-05-13 06:49:11 -0300

    Runner Github

--- src/modules/ec2/data.tf
+++ src/modules/ec2/data.tf
@@ -1,3 +1,7 @@
  data "aws_key_pair" "xcoin_mongodb_key" {
    key_name = "xcoin-mongodb"
  }
+
+ data "aws_key_pair" "github_runner_key" {
+   key_name = "xcoin-mongodb"
+ }
--- src/modules/ec2/ec2.tf
+++ src/modules/ec2/ec2.tf
@@ -11,7 +11,23 @@ resource "aws_instance" "xcoin" {
   ]
 }
 
+resource "aws_instance" "github_runner" {
+  ami                                  = "ami-04b70fa74e45c3917"
+  instance_type                        = "t2.micro"
+  key_name                             = data.aws_key_pair.github_runner_key.key_name
+  subnet_id = "subnet-0bc68bccc25aa8a3f"
+  tags = {
+    "Name" = "XCoin_GithubRunner"
+  }
+  vpc_security_group_ids = [
+    "sg-052f05f0390ce9d26",
+  ]
+}
 
 resource "aws_eip" "mongodb_eip" {
   instance = aws_instance.xcoin.id
+}
+
+resource "aws_eip" "githubrunner_eip" {
+  instance = aws_instance.github_runner
 }
