f24ed2016778088c688e7ef4400511a8989a27b8
Gonzalo Santiago Da Costa <gdacosta@proveedor.teco.com.ar>
2024-05-24 10:13:43 -0300

    Orden pipeline + GB en disco

--- src/modules/ec2/ec2.tf
+++ src/modules/ec2/ec2.tf
@@ -29,6 +29,11 @@ resource "aws_instance" "github_runner_febe" {
   instance_type                        = "t2.medium"
   key_name                             = data.aws_key_pair.github_runner_key.key_name
   subnet_id = "subnet-0bc68bccc25aa8a3f"
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
   tags = {
     "Name" = "XCoin_GithubRunner_febe"
   }
