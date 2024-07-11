62ec87f84e53598b656c3e08ac0c8da7649aa713
Gonzalo Santiago Da Costa <gdacosta@uade.edu.ar>
2024-07-11 13:08:20 -0300

    comento todo

--- README.md
+++ README.md
@@ -9,3 +9,52 @@ Ver si cambia la IP cuando se corta el lab:    ec2-54-88-69-141.compute-1.amazon
 Port: 27017
 
 Mongo funciona.
+
+
+
+
+
+
+
+
+
+resource "aws_sqs_queue" "sqs_queue" {
+
+  name                        = "cryptosqs"
+  delay_seconds               = 10
+  visibility_timeout_seconds  = 30
+  max_message_size            = 2048
+  message_retention_seconds   = 86400
+  receive_wait_time_seconds   = 2
+}
+
+resource "aws_sqs_queue_policy" "sqs_policy" {
+  queue_url = aws_sqs_queue.sqs_queue.url
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "PolicyForSQS",
+    Statement = [
+      {
+        Sid       = "__owner_statement1",
+        Effect    = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::268120344536:root"
+        },
+        Action   = "SQS:*",
+        Resource = "${aws_sqs_queue.sqs_queue.arn}"
+      },
+      {
+        Effect = "Allow",
+        Action = "SQS:SendMessage",
+        Resource = "${aws_sqs_queue.sqs_queue.arn}"
+        Condition = {
+          ArnEquals = {
+              "aws:SourceArn" = "arn:aws:sns:us-east-1:268120344536:criptosnstopic"
+          }
+        }
+      }
+    ]
+  })
+}
+
--- src/main.tf
+++ src/main.tf
@@ -23,65 +23,65 @@ provider "aws" {
 }
 
 
-module "ec2_db" {
-  source = "./modules/ec2"
-}
+# module "ec2_db" {
+#   source = "./modules/ec2"
+# }
 
-module "sqs" {
-  source = "./modules/sqs"
-}
+# module "sqs" {
+#   source = "./modules/sqs"
+# }
 
-module "sns" {
-  source        = "./modules/sns"
-  sqs_queue_arn = module.sqs.sqs_queue_arn
-}
+# module "sns" {
+#   source        = "./modules/sns"
+#   sqs_queue_arn = module.sqs.sqs_queue_arn
+# }
 
-module "s3-tfstate" {
-  source             = "./modules/s3-tfstate"
-  bucket_name        = var.bucket_name
-  dynamodb_table     = var.dynamodb_table
-  bucket_description = var.bucket_description
-}
+# module "s3-tfstate" {
+#   source             = "./modules/s3-tfstate"
+#   bucket_name        = var.bucket_name
+#   dynamodb_table     = var.dynamodb_table
+#   bucket_description = var.bucket_description
+# }
 
-module "ecr" {
-  source = "./modules/ecr"
-}
+# module "ecr" {
+#   source = "./modules/ecr"
+# }
 
-module "ecsCluster" {
-  source = "./modules/ecs"
+# module "ecsCluster" {
+#   source = "./modules/ecs"
 
-  app_cluster_name   = var.app_cluster_name
-  availability_zones = var.availability_zones
-  app_task_famliy    = var.app_task_famliy
-  ecr_repo_url       = module.ecr.repository_url
-  container_port     = var.container_port
-  app_task_name      = var.app_task_name
+#   app_cluster_name   = var.app_cluster_name
+#   availability_zones = var.availability_zones
+#   app_task_famliy    = var.app_task_famliy
+#   ecr_repo_url       = module.ecr.repository_url
+#   container_port     = var.container_port
+#   app_task_name      = var.app_task_name
 
-  application_load_balancer_name = var.application_load_balancer_name
-  target_group_name              = var.target_group_name
-  app_service_name               = var.app_service_name
-}
+#   application_load_balancer_name = var.application_load_balancer_name
+#   target_group_name              = var.target_group_name
+#   app_service_name               = var.app_service_name
+# }
 
-output "ec2_public_ip" {
-  value = module.ec2_db.ec2_public_ip
-}
+# output "ec2_public_ip" {
+#   value = module.ec2_db.ec2_public_ip
+# }
 
-output "sqs_url" {
-  value = module.sqs.sqs_queue_url
-}
+# output "sqs_url" {
+#   value = module.sqs.sqs_queue_url
+# }
 
-output "sqs_arn" {
-  value = module.sqs.sqs_queue_arn
-}
+# output "sqs_arn" {
+#   value = module.sqs.sqs_queue_arn
+# }
 
-output "sns_arn" {
-  value = module.sns.sns_topic_arn
-}
+# output "sns_arn" {
+#   value = module.sns.sns_topic_arn
+# }
 
-output "EIP" {
-  value = module.ec2_db.EIP
-}
+# output "EIP" {
+#   value = module.ec2_db.EIP
+# }
 
-output "ECR_Repository" {
-  value = module.ecr.repository_url
-}
+# output "ECR_Repository" {
+#   value = module.ecr.repository_url
+# }
